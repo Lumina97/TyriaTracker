68e2875516bd74a96d0ec92525915f47695efdef
Lumina <erik04.nivala22@gmail.com>
2024-12-04 22:14:12 -0600

    fixed backend IP

--- src/Utils/settings.ts
+++ src/Utils/settings.ts
@@ -2,4 +2,4 @@ export const minUsernameLength = 4;
 export const minPasswordLength = 4;
 export const APIKeyLength = 17;
 
-export const APIBaseURL = "https://tyriatracker-backend-1.onrender.com/";
+export const APIBaseURL = "https://tyriatracker-backend.onrender.com/";
