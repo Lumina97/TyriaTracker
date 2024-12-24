0bb7f1a945a925e2c6ef39c888769e1ca8b15b24
Lumina <erik04.nivala22@gmail.com>
2024-12-23 22:01:16 -0600

    fixed backend URL

--- src/Utils/settings.ts
+++ src/Utils/settings.ts
@@ -2,7 +2,7 @@ export const minUsernameLength = 4;
 export const minPasswordLength = 4;
 export const APIKeyLength = 17;
 
-export const APIBaseURL = "https://tyriatracker-backend.onrender.com";
+export const APIBaseURL = "https://tyriatracker-backend.onrender.com/";
 //export const APIBaseURL = "http://localhost:3030/";
 
 export const wikiBaseURL = "https://wiki.guildwars2.com/wiki/";
